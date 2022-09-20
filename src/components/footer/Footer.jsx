4c0c85357e7a7952fe5c00e772667e54673095a0
jaolsen7 <jaolsen@berkeley.edu>
2022-09-19 20:48:28 -0700

    footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,21 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">ANGEL RODRIGUEZ</a>
+
+      <ul className="footer__links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__copyright">
+        <small>&copy; Designed by Jason Olsen. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
